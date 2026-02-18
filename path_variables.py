be75bc2eb1289070f66e8aa45eb59adc13f28a48
Ojas Mavinkurve <147416041+ojasmavinkurve@users.noreply.github.com>
2026-02-18 13:09:41 +0530

    Remove BASE_PATH from path_variables.py
    Removed BASE_PATH variable definition.

--- path_variables.py
+++ path_variables.py
@@ -1,7 +1,4 @@
 import os
-
-BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_PATH = os.path.join(BASE_DIR, "data")
 
